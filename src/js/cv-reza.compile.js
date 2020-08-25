5bb7f936b628ac47c1b466ac480b69cc4668fde7
rezafikkri <fikkri.reza@gmail.com>
2020-08-25 19:13:07 +0700

    memperbaiki struktur folder

---
+++ src/js/cv-reza.compile.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*!
+ * CV Reza v2
+ * Copyright (c) 2020 Reza Sariful Fikri
+*/
+var r = function r() {
+  console.log('res');
+};
+
+r();
--- src/js/cv-reza.js
+++ src/js/cv-reza.js
@@ -2,3 +2,8 @@
  * CV Reza v2
  * Copyright (c) 2020 Reza Sariful Fikri
 */
+const r = () => {
+	console.log('res');
+}
+
+r();
