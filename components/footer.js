7fd65a996e931f5f23136799d1fac3b441c69ddf
rezafikkri <fikkri.reza@gmail.com>
2023-02-18 01:01:49 +0700

    feat: create footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="mt-40">
-      <p>&copy;</p>
+    <footer className="mt-28 flex justify-between py-4 text-sm text-gray-400">
+      <p>Copyright &copy; 2023</p>
+      <p>Dibuat dengan <Link href="">Next.js</Link> dan <Link href="">Tailwind CSS</Link></p>
     </footer>
   );
 }
