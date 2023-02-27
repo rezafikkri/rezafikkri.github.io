7fd143ac465febc8e9cc13877998fbedb921ea9d
rezafikkri <fikkri.reza@gmail.com>
2023-02-27 15:30:04 +0700

    refactor: improve footer responsive

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="mt-28 flex justify-between py-4 text-sm text-gray-400">
+    <footer className="mt-20 md:mt-28 flex flex-col md:flex-row md:justify-between py-4 text-sm text-gray-400">
       <p>Copyright &copy; 2023</p>
       <p>Dibuat dengan <Link href="https://nextjs.org" rel="noopener noreferrer" target="_blank">Next.js</Link> dan <Link href="https://tailwindcss.com" rel="noopener noreferrer" target="_blank">Tailwind CSS</Link></p>
     </footer>
