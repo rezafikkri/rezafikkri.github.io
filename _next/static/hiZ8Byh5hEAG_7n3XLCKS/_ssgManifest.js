d215dc69dac858a4dffb9ab85d06fa7d2c389840
rezafikkri <fikkri.reza@gmail.com>
2023-04-04 04:55:28 +0000

    Deploying to gh-pages from @ rezafikkri/rezafikkri.github.io@7253346aac5bc26988bc5d450f1d0004d277b307 🚀

---
+++ _next/static/hiZ8Byh5hEAG_7n3XLCKS/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblogs","\u002Fblogs\u002F[slug]","\u002Fblogs\u002Ftopics\u002F[topic]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  devIndicators: {
-    buildActivity: false
-  }
-}
-
-module.exports = nextConfig
