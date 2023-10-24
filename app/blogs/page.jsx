dda1c0c2bc629b79357042b2f0d0fdf03880fd63
rezafikkri <fikkri.reza@gmail.com>
2023-10-24 20:04:28 +0700

    refactor: metadata for blog

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,6 +1,27 @@
 import BlogsPage from "./blogs-page"; 
 import { getTopics, getPosts } from "../_lib/posts";
 
+const title = "Blog";
+const description = "Kumpulan tulisan mengenai programming dan teknologi secara umum.";
+
+export const metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    url: '/blogs',
+    images: [
+      {
+        url: '/Reza logo.svg',
+        width: 744,
+        height: 744,
+      }, 
+    ],
+    type: 'article',
+  },
+};
+
 export default function Page() {
   const topics = getTopics();
   const posts = getPosts();
