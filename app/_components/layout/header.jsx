8169607c3915c244f06564be08a1c4760fa4bddf
rezafikkri <fikkri.reza@gmail.com>
2023-10-24 08:17:32 +0700

    refactor: header

--- app/_components/layout/header.jsx
+++ app/_components/layout/header.jsx
@@ -3,13 +3,9 @@ import Nav from "./nav";
 
 export default function Header() {
   return (
-    <header className="pt-8 md:pt-12">
-      <Link className="inline-flex no-underline" href="/">
+    <header className="pt-8 md:pt-12 flex justify-between content-center items-center">
+      <Link className="no-underline" href="/">
         <img src="/Reza logo.svg" width={35} height={35} alt="Reza logo" loading="lazy" decoding="async" />
-        <div className="ml-4">
-          <span className={`text-xl text-gray-800 font-bold block leading-none`}>Reza Sariful Fikri</span>
-          <span className={`text-gray-500 leading-none`}>Web Developer</span>
-        </div>
       </Link>
       <Nav />
     </header>
