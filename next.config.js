1beff7d3f78dd66745d906c76a8a511279e78b1b
rezafikkri <fikkri.reza@gmail.com>
2023-10-24 08:44:02 +0700

    fix: next js dev slow

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-let nextConfig = {
-  reactStrictMode: true,
-  devIndicators: {
-    buildActivity: false
-  },
-};
-
-module.exports = nextConfig; 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.jsx',
   ],
   theme: {
     extend: {
