83275336a437f28d8c2f7e7f28506135837099cc
rezafikkri <fikkri.reza@gmail.com>
2023-10-24 08:09:52 +0700

    refactor: footer

--- app/_components/layout/footer.jsx
+++ app/_components/layout/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mt-20 md:mt-28 flex flex-col md:flex-row md:justify-between py-4 text-sm text-gray-500">
       <p>Copyright &copy; 2023</p>
-      <p>Dibuat dengan <Link href="https://nextjs.org">Next.js</Link>, <Link href="https://tailwindcss.com">Tailwind CSS</Link> dan <Link href="https://rstacruz.github.io/nprogress">NProgress.js</Link></p>
+      <p>Dibuat dengan <Link href="https://nextjs.org">Next.js</Link> dan <Link href="https://tailwindcss.com">Tailwind CSS</Link>, oleh Reza Sariful Fikri 🇮🇩🇵🇸</p>
     </footer>
   );
 }
