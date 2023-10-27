4547dcb65e777ea08b71004cf58727833c514e2c
rezafikkri <fikkri.reza@gmail.com>
2023-10-27 16:39:21 +0700

    refactor: metadata for blog topic

--- app/blogs/topic/[topic]/page.jsx
+++ app/blogs/topic/[topic]/page.jsx
@@ -2,6 +2,28 @@ import { getPosts, getTopics } from "@/app/_lib/posts.mjs";
 import PostList from "@/app/_components/post/post-list";
 import LargeTopics from "@/app/_components/post/large-topics";
 
+export function generateMetadata({ params: { topic: selectedTopic } }) {
+  const description = `Kumpulan tulisan mengenai ${selectedTopic}.`;
+
+  return {
+    title: selectedTopic,
+    description: description,
+    openGraph: {
+      title: selectedTopic,
+      description: description,
+      url: `/blogs/topics/${selectedTopic}`,
+      images: [
+        {
+          url: '/Reza logo.svg',
+          width: 744,
+          height: 744,
+        },
+      ],
+      type: 'article',
+    },
+  };
+}
+
 export function generateStaticParams() {
   return getTopics().map(topic => ({ topic }));
 }
