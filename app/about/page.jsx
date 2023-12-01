2e0469e964e31e8f4b2d3761d79c4aca89a84da8
rezafikkri <fikkri.reza@gmail.com>
2023-12-01 09:19:00 +0700

    refactor: change twitter logo to twitter x

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -43,8 +43,9 @@ export default function Page() {
         <p className="mt-1 text-lg">Next.js - MongoDB - Redis - RabbitMQ - Webpack - GIMP - Inkscape - Kdenlive - Linux - Python</p>
 
         <Link href="/Reza CV.pdf" target="_blank" className="mt-5 inline-block no-underline bg-white hover:bg-gray-100 border border-gray-200 px-4 py-2 rounded-lg">
-          <span className="mr-1.5">CV</span>
+          <span className="mr-1.5">Unduh CV</span>
           <svg className="inline-block align-baseline" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg></Link>
       </section>
     </>
-  );}
+  );
+}
