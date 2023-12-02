6909be61752a324c622ba8244ba3795095060a5d
rezafikkri <fikkri.reza@gmail.com>
2023-12-02 23:38:11 +0000

    Deploying to gh-pages from @ rezafikkri/rezafikkri.github.io@bbd196b1affb822a934d049dc8d7c47cdeca62d2 🚀

---
+++ _next/static/9JRlQ5Mmj0bj7pf2sk_oi/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblogs\u002F[slug]","\u002Fblogs\u002Ftopic\u002F[topic]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/n4e5VTtI4wlkqO71KQ1Ei/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblogs","\u002Fblogs\u002F[slug]","\u002Fblogs\u002Ftopics\u002F[topic]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
