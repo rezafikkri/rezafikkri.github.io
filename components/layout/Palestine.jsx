daf305d37d630741b0a878f0ced477cdcb11924d
rezafikkri <fikkri.reza@gmail.com>
2023-12-06 19:11:31 +0700

    refactor: change Palestine component style

--- components/layout/Palestine.jsx
+++ components/layout/Palestine.jsx
@@ -2,14 +2,13 @@ import Link from "next/link";
 
 export default function Palestine() {
   return (
-    <div className="flex justify-center py-3 px-4 bg-gray-100 text-gray-700">
-      <span>Free Palestine 🇵🇸</span>
-      <span className="ms-1">-</span>
+    <div className="sm:text-center py-3 px-4 bg-gray-100 text-gray-700">
+      <span className="me-1">Free Palestine 🇵🇸</span>
       <Link
         href="https://inh.or.id/"
         rel="noopener noreferrer"
         target="_blank"
-        className="group ms-1 no-underline hover:text-ajwa-blue"
+        className="group no-underline hover:text-ajwa-blue inline-block"
       >
         <span className="me-1">Kirim Hadiah Melalui INH</span>
         <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" className="inline-block align-baseline stroke-gray-700 group-hover:stroke-ajwa-blue" viewBox="0 0 16 16"><path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/><path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/></svg>
