d17e6157a208e5ea3386364da01cb6896ba37cd9
rezafikkri <fikkri.reza@gmail.com>
2023-12-08 01:39:38 +0000

    Deploying to gh-pages from @ rezafikkri/rezafikkri.github.io@c7e481290227bd92033f870f6bf08dec13abfe7f 🚀

---
+++ _next/static/chunks/app/page-345182cd44ddf904.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,301,923,895,606],{5199:function(n,e,u){Promise.resolve().then(u.t.bind(u,8326,23))}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=5199)}),_N_E=n.O()}]);
