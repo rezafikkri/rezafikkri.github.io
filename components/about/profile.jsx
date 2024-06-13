a5f0248128fe161b7647750be82c842212ba2793
rezafikkri <fikkri.reza@gmail.com>
2024-06-13 07:57:55 +0700

    feat: remove unuseful attr target _blank

--- components/about/profile.jsx
+++ components/about/profile.jsx
@@ -7,19 +7,19 @@ export default function SocialMedia() {
         <img src="/rezas.jpg" height={150} width={150} alt="Photo Profile" className="rounded-full" loading="lazy" decoding="async" />
       </div>
       <div className="flex gap-2 mt-5">
-        <Link href="https://github.com/rezafikkri" rel="noopener noreferrer" target="_blank" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
+        <Link href="https://github.com/rezafikkri" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
           <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>
         </Link>
-        <Link href="https://twitter.com/fikkrireza" rel="noopener noreferrer" target="_blank" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
+        <Link href="https://twitter.com/fikkrireza" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
           <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 16 16"><path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/></svg>
         </Link>
-        <Link href="https://www.linkedin.com/in/reza-sariful-fikri/" rel="noopener noreferrer" target="_blank" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
+        <Link href="https://www.linkedin.com/in/reza-sariful-fikri/" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
           <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 16 16"><path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/></svg>
         </Link>
-        <Link href="https://web.facebook.com/reza.sariful.fikri" rel="noopener noreferrer" target="_blank" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
+        <Link href="https://web.facebook.com/reza.sariful.fikri" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
           <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 16 16"><path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/></svg>
         </Link>
-        <Link href="https://youtube.com/@rezafikkri" rel="noopener noreferrer" target="_blank" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
+        <Link href="https://youtube.com/@rezafikkri" className="border-2 border-gray-500 hover:border-ajwa-blue rounded-lg px-2.5 py-1.5">
           <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 16 16"><path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/></svg>
         </Link>
       </div>
--- components/project/project-list.jsx
+++ components/project/project-list.jsx
@@ -7,8 +7,8 @@ export default function ProjectList() {
         <h3 className="text-xl font-bold mb-2 text-gray-900">Reza Admin</h3>
         <p className="text-gray-700 mb-5">Templat admin yang dibuat dengan bootstrap 4</p>
         <div className="absolute bottom-4">
-          <Link href="https://github.com/rezafikkri/Reza-Admin" rel="noopener noreferrer" target="_blank" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Code</Link>
-          <Link href="https://rezafikkri.github.io/Reza-Admin" rel="noopener noreferrer" target="_blank" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Demo</Link>
+          <Link href="https://github.com/rezafikkri/Reza-Admin" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Code</Link>
+          <Link href="https://rezafikkri.github.io/Reza-Admin" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Demo</Link>
         </div>
       </div>
 
@@ -16,7 +16,7 @@ export default function ProjectList() {
         <h3 className="text-xl font-bold mb-2 text-gray-900">Manport</h3>
         <p className="text-gray-700 mb-5">Aplikasi pengelola raport siswa SMK dibuat dengan PHP native</p>
         <div className="absolute bottom-4">
-          <Link href="https://github.com/rezafikkri/Manport" rel="noopener noreferrer" target="_blank" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Code</Link>
+          <Link href="https://github.com/rezafikkri/Manport" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Code</Link>
         </div>
       </div>
 
@@ -24,7 +24,7 @@ export default function ProjectList() {
         <h3 className="text-xl font-bold mb-2 text-gray-900">Website Sekolah</h3>
         <p className="text-gray-700 mb-5">Website sekolah yang dibuat dengan Codeigniter 3</p>
         <div className="absolute bottom-4">
-          <Link href="https://github.com/rezafikkri/School-website" rel="noopener noreferrer" target="_blank" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Code</Link>
+          <Link href="https://github.com/rezafikkri/School-website" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Code</Link>
         </div>
       </div>
 
@@ -32,7 +32,7 @@ export default function ProjectList() {
         <h3 className="text-xl font-bold mb-2 text-gray-900">Nvim R</h3>
         <p className="text-gray-700 mb-5">Simpel konfigurasi Neovim untuk Programming</p>
         <div className="absolute bottom-4">
-          <Link href="https://github.com/rezafikkri/Nvim-R" rel="noopener noreferrer" target="_blank" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Code</Link>
+          <Link href="https://github.com/rezafikkri/Nvim-R" className="inline-block no-underline border border-gray-200 bg-gray-50 hover:bg-gray-100 rounded-lg px-2 py-1 text-sm mr-2">Code</Link>
         </div>
       </div>
     </>
