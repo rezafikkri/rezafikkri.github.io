84333e7fe26d20621749298608ed46a0314665f3
rezafikkri <fikkri.reza@gmail.com>
2026-04-05 10:03:56 +0000

    Deploying to gh-pages from @ rezafikkri/rezafikkri.github.io@892e0b82c490fac2fbad85bf5ef92465ab030eda 🚀

---
+++ _next/static/chunks/app/about/page-3ad6e0ad14046b79.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{9698:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3580,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[199,999,427,358],()=>s(9698)),_N_E=e.O()}]);
---
+++ _next/static/chunks/pages/_error-9daa3e623e91527e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{9898:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(9600)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(9898)),_N_E=_.O()}]);
