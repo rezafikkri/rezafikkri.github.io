af720aecca88657893d35aca9525a774035bf143
rezafikkri <fikkri.reza@gmail.com>
2026-04-06 10:46:30 +0000

    Deploying to gh-pages from @ rezafikkri/rezafikkri.github.io@c3b8c525040d96132ea71c66acb9f2e57f0dd52d 🚀

--- _next/static/KhsTWiTokIbGebjrnOU1N/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblogs\u002F[slug]","\u002Fblogs\u002Ftopic\u002F[topic]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/blogs/topics/sitemap.xml/route-f2f2ba6d03de4847.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[506,837],{5266:()=>{}},_=>{_.O(0,[225,736,358],()=>_(_.s=5266)),_N_E=_.O()}]);
